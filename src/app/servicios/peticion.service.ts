c594477f409d33c5032a7e7fab0336911d5e338b
jlopezalcala19 <jlopezalcala1@hotmail.com>
2024-11-09 13:20:48 -0500

    Se actualiza servicio de peticion

--- src/app/servicios/peticion.service.ts
+++ src/app/servicios/peticion.service.ts
@@ -10,32 +10,49 @@ export class PeticionService {
 
   urlLocal:string ="https://backend-aliat.onrender.com"//"http://localhost:3000"
 
-  Post(url:string, data:{}){
-    let promise = new Promise((resolve,reject)=>{
-      this.http.post(url,data)
-      .toPromise()
-      .then((res)=>{
-        resolve(res)
-      }).catch((error)=>{
-        reject(error)
-      })
-    })
-    return promise
-  }
+//   Post(url:string, data:{}){
+//     let promise = new Promise((resolve,reject)=>{
+//       this.http.post(url,data)
+//       .toPromise()
+//       .then((res)=>{
+//         resolve(res)
+//       }).catch((error)=>{
+//         reject(error)
+//       })
+//     })
+//     return promise
+//   }
 
-  Get(url:string, data:{}){
-    let promise = new Promise((resolve,reject)=>{
-      this.http.get(url)
-      .toPromise()
-      .then((res)=>{
-        resolve(res)
-      }).catch((error)=>{
-        reject(error)
-      })
-    })
-    return promise
-  }
+//   Get(url:string, data:{}){
+//     let promise = new Promise((resolve,reject)=>{
+//       this.http.get(url)
+//       .toPromise()
+//       .then((res)=>{
+//         resolve(res)
+//       }).catch((error)=>{
+//         reject(error)
+//       })
+//     })
+//     return promise
+//   }
 
+async Post(url: string, data: {}): Promise<any> {
+  try {
+    const response = await this.http.post(url, data, { withCredentials: true }).toPromise();
+    return response;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async Get(url: string): Promise<any> {
+  try {
+    const response = await this.http.get(url, { withCredentials: true }).toPromise();
+    return response;
+  } catch (error) {
+    throw error;
+  }
+}
 
 
 }
